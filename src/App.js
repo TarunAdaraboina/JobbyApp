975c91b27fc8234e2c937961e242e35362b17c7c
TarunAdaraboina <tarunadaraboina@gamil.com>
2024-07-24 14:30:00 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import {Component} from 'react'
 import {Route, Switch, Redirect} from 'react-router-dom'
-import Login from './Components/Login'
-import Home from './Components/Home'
-import Jobs from './Components/Jobs'
 
-import JobItemDetails from './Components/JobItemDetails'
-import NotFound from './Components/NotFound'
-import ProtectedRoute from './Components/ProtectedRoute'
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Jobs from './components/Jobs'
+import JobItemDetails from './components/JobItemDetails'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 
 import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <ProtectedRoute exact path="/" component={Home} />
-        <ProtectedRoute exact path="/jobs" component={Jobs} />
-        <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
-        <Route path="/not-found" component={NotFound} />
-        <Redirect to="not-found" />
-      </Switch>
-    )
-  }
-}
+const App = () => (
+  <>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/jobs" component={Jobs} />
+      <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+      <Route path="/not-found" component={NotFound} />
+      <Redirect to="not-found" />
+    </Switch>
+  </>
+)
 
 export default App
